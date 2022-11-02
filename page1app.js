d6fb0a38d77b2bae0f107e543fc48cf07c3f7446
Chester Vincent <chetvincent@chesters-mbp-2.mynetworksettings.com>
2022-11-01 23:03:46 -0400

    hover-over works, made clickable div buttons

--- page1app.js
+++ page1app.js
@@ -1,5 +1,88 @@
+
 $(() => {
 
-// const $test = $('<h1>').text(`Chester Vincent's Portfolio`).css('color', 'blue').appendTo('body');
 
+
+
+
+
+///////// CLICK FUNCTIONS /////////
+
+  const navigateHome = () => {
+    alert('works!')
+  }
+  const navigateAboutMe = () => {
+    alert('works!')
+  }
+  const navigateResume = () => {
+    alert('works!')
+  }
+  const navigateProjects = () => {
+    alert('works!')
+  }
+
+///////////////
+
+
+
+
+
+
+
+
+
+
+//////// CLICK LISTENERS ////////
+
+$('#headerClick').on('click', navigateHome);
+$('#aboutMeClick').on('click', navigateAboutMe);
+$('#resumeClick').on('click', navigateResume);
+$('#projectsClick').on('click', navigateProjects);
+
+///////// HOVER-OVER LISTENERS //////////
+
+$('.headerfg').hover(function() {
+      $('.headerbg').css('background-color', 'rgb(225, 225, 225, .5)');
+    }, function() {
+      $('.headerbg').css('background-color', 'rgb(73, 93, 78, .5)')
+    });
+
+$('.linksfg').hover(function() {
+       $('.linksbg').css('background-color', 'rgb(225, 225, 225, .5)');
+    }, function() {
+       $('.linksbg').css('background-color', 'rgb(73, 93, 78, .5)')
+    });
+
+$('.carouselfg').hover(function() {
+       $('.carouselbg').css('background-color', 'rgb(225, 225, 225, .5)');
+    }, function() {
+       $('.carouselbg').css('background-color', 'rgb(73, 93, 78, .5)')
+    });
+
+$('.link_1_fg').hover(function() {
+       $('.link_1_bg').css('background-color', 'rgb(225, 225, 225, .5)');
+    }, function() {
+       $('.link_1_bg').css('background-color', 'rgb(73, 93, 78, .5)')
+    });
+
+$('.link_2_fg').hover(function() {
+       $('.link_2_bg').css('background-color', 'rgb(225, 225, 225, .5)');
+    }, function() {
+       $('.link_2_bg').css('background-color', 'rgb(73, 93, 78, .5)')
+    });
+
+$('.link_3_fg').hover(function() {
+       $('.link_3_bg').css('background-color', 'rgb(225, 225, 225, .5)');
+    }, function() {
+       $('.link_3_bg').css('background-color', 'rgb(73, 93, 78, .5)')
+    });
+
+$('.footerfg').hover(function() {
+       $('.footerbg').css('background-color', 'rgb(225, 225, 225, .5)');
+    }, function() {
+       $('.footerbg').css('background-color', 'rgb(73, 93, 78, .5)')
+    });
+
+
+/////// END OF JS ////////
 });
