1833f684d6c19b976732a5b6c2364ad256a800f1
Chester Vincent <chetvincent@chesters-mbp-2.mynetworksettings.com>
2022-11-03 15:17:28 -0400

    finished photo modal-carousel and finished media query for homepage

--- page1app.js
+++ page1app.js
@@ -9,6 +9,13 @@ $(() => {
   const $carousel = $('#carouselModal');
   const $closeCarousel = $('#closeCarousel');
 
+  let numberOfImages = $('.images').children().length - 1;
+  let currentImg = 0;
+
+
+
+
+
 
 
 
@@ -29,6 +36,28 @@ $(() => {
   const navigateProjects = () => {
     alert('works!')
   }
+
+  ////// CAROUSEL MODAL ///////
+
+  $('#previousbtn').on('click', () => {
+      $('.images').children().eq(currentImg).css('display', 'none');
+      if (currentImg > 0) {
+        currentImg--
+      } else {
+        currentImg = numberOfImages;
+      }
+     $('.images').children().eq(currentImg).css('display', 'block');
+  })
+
+  $('#nextbtn').on('click', () => {
+      $('.images').children().eq(currentImg).css('display', 'none');
+      if (currentImg < numberOfImages) {
+        currentImg++
+      } else {
+        currentImg = 0;
+      }
+     $('.images').children().eq(currentImg).css('display', 'block');
+  })
 
 ///////////////
 
