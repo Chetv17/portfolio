bd8862481b860ee7cee8872219380570cfd24bd1
Chester Vincent <chetvincent@chesters-mbp-2.mynetworksettings.com>
2022-11-04 16:28:07 -0400

    page 4 projects page started, started a psychedelic cabin game

---
+++ page4app.js
@@ -0,0 +1,73 @@
+$(() => {
+
+///////// CLICK FUNCTIONS /////////
+
+const $openBtn = $('#openModal');
+const $modal = $('#contactModal');
+const $closeBtn = $('#close');
+
+
+
+    const navigateHome = () => {
+      alert('works!')
+    }
+
+
+
+
+
+    const openModal = () => {
+  $modal.css('display', 'block');
+}
+
+const closeModal = () => {
+  $modal.css('display', 'none');
+}
+
+
+
+
+
+
+
+
+
+//////// INITIAL CONDITION WHERE BIO TEXT IS HIDDEN ////
+
+$('.bio_text').hide();
+
+
+
+
+
+
+
+//////// CLICK LISTENERS ////////
+
+$('#headerClick').on('click', navigateHome);
+
+$openBtn.on('click', openModal);
+
+$closeBtn.on('click', closeModal);
+
+
+
+
+////////// HOVER OVER LISTENERS /////////
+
+
+
+///BACKGROUND EFFECTS
+$('.headerfg').hover(function() {
+      $('.headerbg').css('background-color', 'rgb(225, 225, 225, .1)');
+    }, function() {
+      $('.headerbg').css('background-color', 'rgb(73, 93, 78, .1)')
+    });
+
+$('.footerfg').hover(function() {
+      $('.footerbg').css('background-color', 'rgb(225, 225, 225, .1)');
+    }, function() {
+      $('.footerbg').css('background-color', 'rgb(73, 93, 78, .1)')
+    });
+
+});
