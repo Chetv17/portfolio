b66040be6c63ff21ee0ba9bb4af5938cb4c69785
Chester Vincent <chetvincent@chesters-mbp-2.mynetworksettings.com>
2022-11-05 16:08:28 -0400

    working on psychedelic cabin game

--- page4app.js
+++ page4app.js
@@ -1,10 +1,21 @@
 $(() => {
 
+
+const cabinObjects = ['map to cabin'];
+
 ///////// CLICK FUNCTIONS /////////
 
 const $openBtn = $('#openModal');
 const $modal = $('#contactModal');
 const $closeBtn = $('#close');
+
+
+const $openCabinCarousel = $('#openCabinCarousel');
+const $cabinCarousel = $('#cabinCarousel');
+const $closeCabinCarousel = $('#closeCabinCarousel');
+
+let numberOfImages = $('.cabin_images').children().length - 1;
+let currentImg = 0;
 
 
 
@@ -24,11 +35,23 @@ const closeModal = () => {
   $modal.css('display', 'none');
 }
 
+const openCabinCarousel = () => {
+$('.door').hide();
+$cabinCarousel.css('display', 'block');
+}
 
+const closeCabinCarousel = () => {
+$('.door').show();
+$cabinCarousel.css('display', 'none');
+}
 
+const openDrawer = () => {
+  $('#potion').css('display', 'block');
+}
 
-
-
+const addPotion = () => {
+  $
+}
 
 
 
@@ -38,6 +61,74 @@ $('.bio_text').hide();
 
 
 
+///////// CABIN GAME ///////////////
+
+////// arrow buttons to scroll photo carousel ///
+
+$('#previousbtn').on('click', () => {
+    $('.cabin_images').children().eq(currentImg).css('display', 'none');
+    if (currentImg > 0) {
+      currentImg--
+    } else {
+      currentImg = numberOfImages;
+    }
+   $('.cabin_images').children().eq(currentImg).css('display', 'block');
+
+})
+
+$('#nextbtn').on('click', () => {
+    $('.cabin_images').children().eq(currentImg).css('display', 'none');
+    if (currentImg < numberOfImages) {
+      currentImg++
+    } else {
+      currentImg = 0;
+    }
+   $('.cabin_images').children().eq(currentImg).css('display', 'block');
+
+})
+
+///// use arrow buttons to make secrets appear in the cabin //////
+
+//previous
+
+///drawer
+$('#previousbtn').on('click', () => {
+  $('.cabin_images').children().eq(currentImg);
+  if (currentImg === 2) {
+    $('#drawer').css('display', 'block');
+    } else {
+      $('#drawer').css('display', 'none');
+    }
+})
+
+$('#nextbtn').on('click', () => {
+  $('.cabin_images').children().eq(currentImg);
+  if (currentImg === 2) {
+    $('#drawer').css('display', 'block');
+    } else {
+      $('#drawer').css('display', 'none');
+    }
+})
+
+/// fireplace
+
+$('#previousbtn').on('click', () => {
+  $('.cabin_images').children().eq(currentImg);
+  if (currentImg === 0) {
+    $('#fireplace').css('display', 'block');
+    } else {
+      $('#fireplace').css('display', 'none');
+    }
+})
+
+$('#nextbtn').on('click', () => {
+  $('.cabin_images').children().eq(currentImg);
+  if (currentImg === 0) {
+    $('#fireplace').css('display', 'block');
+    } else {
+     $('#fireplace').css('display', 'none');
+    }
+})
 
 
 
@@ -45,12 +136,13 @@ $('.bio_text').hide();
 //////// CLICK LISTENERS ////////
 
 $('#headerClick').on('click', navigateHome);
-
 $openBtn.on('click', openModal);
-
 $closeBtn.on('click', closeModal);
+$('#doorClick').on('click', openCabinCarousel);
+$closeCabinCarousel.on('click', closeCabinCarousel);
+$('#drawer').on('click', openDrawer);
 
-
+$('#potion').on('click', addPotion);
 
 
 ////////// HOVER OVER LISTENERS /////////
@@ -69,5 +161,11 @@ $('.footerfg').hover(function() {
     }, function() {
       $('.footerbg').css('background-color', 'rgb(73, 93, 78, .1)')
     });
+
+    $('.cabinCarouselfg').hover(function() {
+           $('.cabinCarouselbg').css('background-color', 'rgb(225, 225, 225, .8)');
+        }, function() {
+           $('.cabinCarouselbg').css('background-color', 'rgb(73, 93, 78, .1)')
+        });
 
 });
